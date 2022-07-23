020d04c313b8d541d0a664e6f7c47930a8907ad9
loveday515 <lovedayikegbulam515@gmail.com>
2022-07-23 16:08:21 +0100

    Initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '3vfw0gtpr4';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-z5trmua7.us.auth0.com',
+  clientId: 'O5c8IuD95SfcBRuZ7rJduSCDjLMh8l51',
+  callbackUrl: 'http://localhost:3000/callback'
+};
